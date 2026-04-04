1683b5a34ff3dd7824fa8492d765c20349b7e15b
R99space99(Tweu/tinnaphat) <r99.99yt@gmail.com>
2026-04-04 23:52:13 +0700

    Update ozoneWall.js

--- scripts/ozoneWall.js
+++ scripts/ozoneWall.js
@@ -1,9 +1,10 @@
-print('Ozone wall started');
 Events.on(ContentInitEvent, function(){
 
-print('ozone wall loaded in');
+    print("ozone wall loaded in");
 
-    const OzoneWall = extend(Wall, "pluscontent-ozone-wall", {
+    const OzoneWall = Vars.content.getByName(ContentType.block, "pluscontent-ozone-wall");
+
+    Object.assign(OzoneWall, {
 
         canPlaceOn(tile){
             if(tile == null) return false;
@@ -16,21 +17,6 @@ print('ozone wall loaded in');
                 }
             }
             return true;
-        },
-
-        build(tile){
-            if(tile == null) return;
-
-            this.super$build(tile);
-
-            for(var dx = 0; dx < 4; dx++){
-                for(var dy = 0; dy < 4; dy++){
-                    var t = Vars.world.tile(tile.x + dx, tile.y + dy);
-                    if(t != null){
-                        t.setBlock(this);
-                    }
-                }
-            }
         },
 
         update(tile){
@@ -76,42 +62,6 @@ print('ozone wall loaded in');
                     }
                 });
             }
-        },
-
-        draw(tile){
-            this.super$draw(tile);
-
-            var ozoneAmount = 0;
-            if(tile.liquids != null){
-                ozoneAmount = tile.liquids.get(Liquids.ozone);
-            }
-
-            var hasPower = false;
-            if(tile.power != null){
-                var p = tile.power.status;
-                if(p > 0.0001){
-                    hasPower = true;
-                }
-            }
-
-            if(ozoneAmount > 0 && hasPower){
-
-                Draw.color(Color.cyan, Color.white, ozoneAmount / 20);
-
-                var region = Core.atlas.find("pluscontent-ozone-wall");
-
-                if(region != null){
-                    Draw.rect(
-                        region,
-                        (tile.x + 2) * Vars.tilesize,
-                        (tile.y + 2) * Vars.tilesize,
-                        Vars.tilesize * 4,
-                        Vars.tilesize * 4
-                    );
-                }
-
-                Draw.reset();
-            }
         }
     });
 
@@ -122,7 +72,5 @@ print('ozone wall loaded in');
     OzoneWall.consumesPower = true;
     OzoneWall.powerConsumption = 2;
 
-    OzoneWall.health = 2000;
-    OzoneWall.category = Category.defense;
-    print('ozone wall prop defined');
+    print("ozone wall patched");
 });
